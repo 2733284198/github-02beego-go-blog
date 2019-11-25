05b5e1ff3d6ab0fd2b956b5b3eee075a16bd8d05
ZH1168\Administrator <491126240@qq.com>
2019-11-25 10:12:50 +0800

    修复统一配置

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/config"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/sirupsen/logrus"
 	"html/template"
 	_ "leechan.inline/routers"
 	"leechan.inline/utils"
@@ -12,8 +14,22 @@ import (
 var TplFuncMap = make(template.FuncMap)
 
 func init() {
+	conf, err := config.NewConfig("ini", "conf/app.conf")
+
+	if err != nil {
+		logrus.Fatalf(err.Error())
+	}
+
+	dbUser := conf.String("db::dbUser")
+	dbPass := conf.String("db::dbPass")
+	dbHost := conf.String("db::dbHost")
+	dbPort := conf.String("db::dbPort")
+	dbName := conf.String("db::dbName")
+
+	dbStr := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName+ "?charset=utf8&loc=Asia%2FShanghai"
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "xxxx:xxxx@tcp(127.0.0.1:3306)/blog?charset=utf8&loc=Asia%2FShanghai")
+
+	orm.RegisterDataBase("default", "mysql", dbStr)
 
 	beego.AddFuncMap("IndexForOne", utils.IndexForOne)
 	beego.AddFuncMap("IndexAddOne",utils.IndexAddOne)
