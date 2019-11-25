8076aa9d2549d0a1ce422da135d41ead6c578a93
ZH1168\Administrator <491126240@qq.com>
2019-11-25 09:54:53 +0800

    修复模板函数用法

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
+	"html/template"
 	_ "leechan.inline/routers"
+	"leechan.inline/utils"
 )
+
+var TplFuncMap = make(template.FuncMap)
 
 func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "xxxx:xxxxxx@tcp(127.0.0.1:3306)/blog?charset=utf8&loc=Asia%2FShanghai")
+	orm.RegisterDataBase("default", "mysql", "xxxx:xxxx@tcp(127.0.0.1:3306)/blog?charset=utf8&loc=Asia%2FShanghai")
+
+	beego.AddFuncMap("IndexForOne", utils.IndexForOne)
+	beego.AddFuncMap("IndexAddOne",utils.IndexAddOne)
+	beego.AddFuncMap("IndexDecrOne",utils.IndexDecrOne)
+	beego.AddFuncMap("StringReplace",utils.StringReplace)
+
 }
 
 func main() {
