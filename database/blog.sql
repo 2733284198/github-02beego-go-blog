d1c4f873b8058d52dd58579e3be79e657032dfe6
chenli <1920853199@qq.com>
2019-12-20 09:00:56 +0800

    Add files via upload

---
+++ database/blog.sql
@@ -0,0 +1,176 @@
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL COMMENT 'ID',
+  `user_id` int(11) DEFAULT '0' COMMENT '用户ID',
+  `title` varchar(255) NOT NULL DEFAULT '' COMMENT '标题',
+  `category_id` int(11) NOT NULL COMMENT '分类ID',
+  `tag` varchar(255) NOT NULL DEFAULT '' COMMENT 'Tag',
+  `remark` varchar(500) NOT NULL DEFAULT '' COMMENT '摘要',
+  `desc` longtext NOT NULL COMMENT '详情',
+  `pv` int(255) DEFAULT '0' COMMENT 'px',
+  `created` datetime DEFAULT NULL COMMENT '创建时间',
+  `updated` datetime DEFAULT NULL COMMENT '更新时间',
+  `status` int(11) DEFAULT '1' COMMENT '1可用，2禁用，3删除',
+  `review` int(11) DEFAULT '0' COMMENT '评论'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL COMMENT '主键',
+  `name` varchar(255) DEFAULT NULL,
+  `pid` int(11) DEFAULT '0' COMMENT '父ID',
+  `sort` int(11) DEFAULT '0' COMMENT '排序',
+  `status` int(11) DEFAULT '1' COMMENT '状态1正常，2删除'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `log`
+--
+
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL COMMENT 'ID',
+  `ip` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `create` datetime NOT NULL,
+  `user_agent` varchar(255) NOT NULL,
+  `page` varchar(255) DEFAULT NULL,
+  `uri` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL COMMENT 'ID',
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '名字',
+  `review` varchar(500) NOT NULL DEFAULT '' COMMENT '评论',
+  `reply` varchar(500) NOT NULL COMMENT '回复',
+  `site` varchar(500) NOT NULL COMMENT '网址',
+  `created` datetime DEFAULT NULL COMMENT '创建时间',
+  `updated` datetime DEFAULT NULL COMMENT '回复时间',
+  `status` int(11) DEFAULT '1' COMMENT '1可用，2禁用'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `review`
+--
+
+CREATE TABLE `review` (
+  `id` int(11) NOT NULL COMMENT 'ID',
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '名字',
+  `review` varchar(500) NOT NULL DEFAULT '' COMMENT '评论',
+  `reply` varchar(500) NOT NULL COMMENT '回复',
+  `site` varchar(500) NOT NULL COMMENT '网址',
+  `created` datetime DEFAULT NULL COMMENT '创建时间',
+  `updated` datetime DEFAULT NULL COMMENT '回复时间',
+  `status` int(11) DEFAULT '1' COMMENT '1可用，2禁用',
+  `article_id` int(11) DEFAULT '1' COMMENT '文章ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL COMMENT 'ID',
+  `name` varchar(255) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `status` int(11) DEFAULT '1' COMMENT '状态'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 表的索引 `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 表的索引 `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 表的索引 `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 表的索引 `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 表的索引 `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID';
+
+--
+-- 使用表AUTO_INCREMENT `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键';
+
+--
+-- 使用表AUTO_INCREMENT `log`
+--
+ALTER TABLE `log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID';
+
+--
+-- 使用表AUTO_INCREMENT `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID';
+
+--
+-- 使用表AUTO_INCREMENT `review`
+--
+ALTER TABLE `review`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID';
+
+--
+-- 使用表AUTO_INCREMENT `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID';
+COMMIT;
+
