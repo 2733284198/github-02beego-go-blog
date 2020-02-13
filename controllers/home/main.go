671a3786474b7010800824e1cb9ca8f2c8c30861
ZH1168\Administrator <491126240@qq.com>
2020-02-13 17:10:50 +0800

    首页推荐

--- controllers/home/main.go
+++ controllers/home/main.go
@@ -1,6 +1,11 @@
 package home
 
-import "github.com/astaxie/beego"
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"go-blog/models/admin"
+)
 
 type MainController struct {
 	BaseController
@@ -8,6 +13,13 @@ type MainController struct {
 
 func (c *MainController) Get() {
 	c.Log("index")
+
+	// 推荐
+	o := orm.NewOrm()
+	var list []*admin.Article
+	o.QueryTable(new(admin.Article)).Filter("status", 1).Filter("recommend", 1).Filter("User__Name__isnull", false).Filter("Category__Name__isnull", false).OrderBy("-id").RelatedSel().All(&list, "id", "title")
+	c.Data["Recommend"] = list
+
 	c.TplName = "home/" + beego.AppConfig.String("view") + "/index.html"
 }
 
